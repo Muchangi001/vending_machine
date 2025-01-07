32b0ec8190545d3180d9b539df61e4abf5c3cef8
Muchangi001 <andrewmuchangi254@gmail.com>
2025-01-08 01:19:48 +0300

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-#!/home/andrew/python/pywbvw/.venv/bin/python
 import webview
 from datetime import datetime
 
@@ -23,5 +22,5 @@ class Api:
         }
     
 api = Api()
-webview.create_window("muchangi", url="static/home.html", width=1024, height=600, js_api=api)
+webview.create_window("muchangi", url="http://127.0.0.1:5500/frontend/static/home.html", width=1024, height=600, js_api=api, frameless=True)
 webview.start()
